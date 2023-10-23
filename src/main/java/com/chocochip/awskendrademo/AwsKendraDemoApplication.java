a28bd1caac5829615a765fa3a14390ca4ea7c288
Kwoun Ki Ho <fingercut3822@gmail.com>
2023-10-24 00:46:36 +0900

    init: Create project

---
+++ src/main/java/com/chocochip/awskendrademo/AwsKendraDemoApplication.java
@@ -0,0 +1,13 @@
+package com.chocochip.awskendrademo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsKendraDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwsKendraDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/chocochip/awskendrademo/AwsKendraDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chocochip.awskendrademo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsKendraDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
